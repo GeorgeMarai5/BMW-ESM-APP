7e79c2047b5000f638489e7b0e095d30276168ba
Albertus <u20487755@tuks.co.za>
2022-05-07 17:52:03 +0200

    create account

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
       },
       {
         path: 'create-account',
-        loadChildren: () => import('../addclientaccount/addclientaccount.module').then(m => m.AddclientaccountPageModule)
+        loadChildren: () => import('../create-account/create-account.module').then(m => m.CreateAccountPageModule)
       },
       {
         path: '',
