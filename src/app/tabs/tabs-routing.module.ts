08d65688cb9bde06e88df51e0383a84c4aebcd09
GeorgeMarai5 <u20774312@tuks.co.za>
2022-05-08 12:44:36 +0200

    updates to logi and password reset

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -19,6 +19,22 @@ const routes: Routes = [
         path: 'create-account',
         loadChildren: () => import('../create-account/create-account.module').then(m => m.CreateAccountPageModule)
       },
+      {
+        path: 'dashboard/client',
+        loadChildren: () => import('../dashboard-client/dashboard-client.module').then( m => m.DashboardClientPageModule)
+      },
+      {
+        path: 'searchemployeeaccount',
+        loadChildren: () => import('../searchemployeeaccount/searchemployeeaccount.module').then( m => m.SearchemployeeaccountPageModule)
+      },
+      {
+        path: 'password-reset',
+        loadChildren: () => import('../password-reset/password-reset.module').then( m => m.PasswordResetPageModule)
+      },
+      {
+        path: 'verify-email',
+        loadChildren: () => import('../verify-email/verify-email.module').then( m => m.VerifyEmailPageModule)
+      },
     ]
   },
   {
