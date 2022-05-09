aed7a18e1a9b6692c2b67ff148f348402e47f4af
GeorgeMarai5 <u20774312@tuks.co.za>
2022-05-09 06:33:47 +0200

    Revert "Merge branch 'master' of https://github.com/GeorgeMarai5/BMW-ESM-APP"
    This reverts commit d1a1026b3e71027abf26c013daee665452a8589a, reversing
    changes made to 8dfeed8a4c93eca00af1ce83c54efa17ee3ffb5f.

--- src/app/update-client/update-client.page.ts
+++ src/app/update-client/update-client.page.ts
@@ -1,84 +1,36 @@
-import { Component, OnInit,ElementRef, Input, Output, NgZone } from '@angular/core';
-import { FormBuilder,Validators,FormGroup, AnyForUntypedForms } from '@angular/forms';
-import { AngularDelegate } from '@ionic/angular';
-import { Observable } from 'rxjs';
-import { ToastController } from '@ionic/angular';
-import { Router,Route } from '@angular/router';
-import { PostService } from '../services/post.service';
-import { getApp } from 'firebase/app';
-import {getFirestore, collection,onSnapshot, addDoc, doc,setDoc, QuerySnapshot} from 'firebase/firestore'
-import { Clients } from '../models/Clients';
-import { ActivatedRoute } from '@angular/router';
-import { snapshotChanges } from '@angular/fire/compat/database';
-import { ClientService } from '../services/Client.service';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-
 
 @Component({
   selector: 'app-update-client',
   templateUrl: './update-client.page.html',
   styleUrls: ['./update-client.page.scss'],
 })
-
 export class UpdateClientPage implements OnInit {
 
-  
-    clientList = [];
-    
-    clients: Clients;
-    clientform : FormGroup;
-  
-  constructor(public clientService: ClientService , private zone: NgZone,private toastCtrl: ToastController,private service: PostService, 
-    public fb: FormBuilder,private router: Router, private route: ActivatedRoute, public authService: AuthService, private firestore: AngularFirestore) { 
+  constructor(public authService: AuthService) { }
 
-this.clients = {} as Clients;
-
-    }
-
-
-
-
-    ngOnInit() {
-
-      this.clientform = this.fb.group({
-        Title: [''],
-        FirstName: [''],
-        LastName: [''],
-        PhoneNumber: [''],
-        Email: [''],
-        Address: [''],
-      })
-
-
-this.clientService.read_Clients().subscribe(data =>{
-
-
-  this.clientList = data.map(e =>{
-
-return{
-
-id: e.payload.doc.id,
-Title: e.payload.doc.data()['Title'],
-FirstName: e.payload.doc.data()['FirstName'],
-LastName: e.payload.doc.data()['lastName'],
-PhoneNumber: e.payload.doc.data()['phone'],
-Email: e.payload.doc.data()['email'],
-Address: e.payload.doc.data()['Address'],
-};
-
-})
-console.log(this.clientList);
-
-
-});
-
-
-    }
-
-
+  ngOnInit() {
   }
 
 
+  /*let field = document.getElementById("field");
+
+  
+  if (sessionStorage.getItem("autosave")) {
+    
+    field.value = sessionStorage.getItem("autosave");
+  }
+  
+  
+  field.addEventListener("change", function() {
+   
+    sessionStorage.setItem("autosave", field.value);
+  });
+
+*/
 
 
+
+
+}
