979b401fab7c61f6d8fd1c5535f5c403169f0c1c
Albertus <u20487755@tuks.co.za>
2022-05-25 19:47:23 +0200

    viewFleet
    viewFleet

--- src/app/view-fleet/view-fleet.page.ts
+++ src/app/view-fleet/view-fleet.page.ts
@@ -1,4 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormBuilder,Validators,FormGroup, FormControl } from '@angular/forms';
+import{FleetService} from '../services/fleet.service';
+
+
+
+interface FleetData {
+  FleetID: number;
+  FleetName: string;
+  FleetLocation: string;
+  FleetVehicleQty: number;
+  
+}
+
+interface FleetVehicles{
+  VehicleID: number;
+  VinNumber: string;
+  ModelName: string;
+  Year: string;
+}
+
 
 @Component({
   selector: 'app-view-fleet',
@@ -7,9 +28,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewFleetPage implements OnInit {
 
-  constructor() { }
+   fleetList = [];
+   fleetData: FleetData;
+   fleetForm: FormGroup;
+
+  constructor(public authService: AuthService,public fb: FormBuilder, private fleetservice:FleetService) { 
+
+    this.fleetData = {} as FleetData;
+
+  }
 
   ngOnInit() {
-  }
+
+    this.fleetForm = this.fb.group({
+      VehicleID: ['', [Validators.required]],
+      VinNumber: ['', [Validators.required]],
+      ModelName: ['', [Validators.required]],
+      Year: ['', [Validators.required]],
+  });
+
+
+  this.fleetservice.read_Fleet().subscribe(data => {
+
+    this.fleetList = data.map(e => {
+      return {
+        id: e.payload.doc.id,
+        isEdit: false,
+        FleetID: e.payload.doc.data()['FleetID'],
+        FleetVehicleQty: e.payload.doc.data()['FleetVehicleQty'],
+        FleetName: e.payload.doc.data()['FleetName'],
+        FleetLocation: e.payload.doc.data()['FleetLocation']
+      };
+    })
+    console.log(this.fleetList);
+
+  });
+
+
+}
+
 
 }
