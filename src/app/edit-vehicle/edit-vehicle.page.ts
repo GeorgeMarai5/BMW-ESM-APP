4acbd2ba7d9cc81d918640454cbe069b09b1db69
RaymondKockott <u19011972@tuks.co.za>
2022-05-29 14:13:35 +0200

    updates
    making changes to the typescript

--- src/app/edit-vehicle/edit-vehicle.page.ts
+++ src/app/edit-vehicle/edit-vehicle.page.ts
@@ -1,70 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
-import { AuthService } from '../services/auth.service';
-import { VehicleService } from '../services/vehicle.service';
-import { Vehicle } from '../models/Vehicle';
-import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import { ActivatedRoute, Router } from '@angular/router';
+import { Dealership } from '../models/Dealership';
+import { AuthService } from '../services/auth.service';
+import { DealershipService } from '../services/dealership.service';
 
 @Component({
-  selector: 'app-edit-vehicle',
-  templateUrl: './edit-vehicle.page.html',
-  styleUrls: ['./edit-vehicle.page.scss'],
+  selector: 'app-edit-dealership',
+  templateUrl: './edit-dealership.page.html',
+  styleUrls: ['./edit-dealership.page.scss'],
 })
+export class EditDealershipPage implements OnInit {
 
-export class EditVehiclePage implements OnInit {
-
-  vehicles: Vehicle;
-  vehicle = {};
-  editVehicleForm: FormGroup;
+  dealerships: Dealership;
+  dealership = {};
+  editDealershipForm: FormGroup;
   isSubmitted = false;
   data: any;
-  
+
   constructor(private route: ActivatedRoute, public fb: FormBuilder, public authService: AuthService, 
-    public service: VehicleService, public firestore: AngularFirestore, public router: Router) {
+    public service: DealershipService, public firestore: AngularFirestore, public router: Router) {
       this.route.params.subscribe(params => {
           this.data = params.id;
       });
-      this.editVehicleForm = new FormGroup({
-        Registration: new FormControl('', Validators.required),
-        VINNum: new FormControl('', [Validators.required, Validators.min(17), Validators.max(17)]),
-        vehicleModel: new FormControl('', Validators.required),
-        warrantyPlan: new FormControl('', Validators.required)
-      })
-     }
-     
+    this.editDealershipForm = new FormGroup({
+      dealershipName: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required)
+    })
+  }
+
   submitForm(){
     this.isSubmitted = true;
-    if(!this.editVehicleForm.valid){
+    if(!this.editDealershipForm.valid){
       return false;
     }
     else{
-        const vehicle = {
-          VehicleModel: this.editVehicleForm.get('vehicleModel').value,
-          Registration: this.editVehicleForm.get('Registration').value,
-          VIN_Number: this.editVehicleForm.get('VINNum').value,
-          Warranty: this.editVehicleForm.get('warrantyPlan').value
+        const dealership = {
+          DealershipName: this.editDealershipForm.get('dealershipName').value,
+          AddressName: this.editDealershipForm.get('address').value
         }
-        this.service.updateVehicle(this.data, vehicle)
+        this.service.updateDealership(this.data, dealership)
         alert("Vehicle was successfully updated.");
       }
-      this.router.navigate(['/tabs/view/vehicle', this.data]);
+      this.router.navigate(['/tabs/view/dealership', this.data]);
   }
 
   ngOnInit() {
-    this.service.getVehicle(this.data).valueChanges()
+    this.service.getDealership(this.data).valueChanges()
     .subscribe(res =>{
     console.log(res)
-    this.editVehicleForm.setValue({
-      vehicleModel: res['VehicleModel'], 
-      Registration: res['Registration'],
-      VINNum: res['VIN_Number'], 
-      warrantyPlan: res['Warranty']
+    this.editDealershipForm.setValue({
+      dealershipName: res['DealershipName'], 
+      address: res['AddressName']
     })
     });
   }
+
   get errorControl() {
-    return this.editVehicleForm.controls;
+    return this.editDealershipForm.controls;
   }
 
 }
