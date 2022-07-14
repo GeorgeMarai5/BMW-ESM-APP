01e9dabca9a6dfbbb27e291dac893910219defc5
GeorgeMarai5 <u20774312@tuks.co.za>
2022-07-14 14:43:38 +0200

    Creating The Step Progress Bar

--- src/app/capture-initial-inspection-details/capture-initial-inspection-details.page.ts
+++ src/app/capture-initial-inspection-details/capture-initial-inspection-details.page.ts
@@ -12,7 +12,6 @@ import { VehicleService } from '../services/vehicle.service';
 })
 export class CaptureInitialInspectionDetailsPage implements OnInit {
 
-  addVehicleForm: FormGroup;
   isSubmitted = false;
   data: any;
 
@@ -21,49 +20,9 @@ export class CaptureInitialInspectionDetailsPage implements OnInit {
     this.route.params.subscribe(params => {
       this.data = params['id'];
     });
-    this.addVehicleForm = new FormGroup({
-      VINNum: new FormControl('', [Validators.required, Validators.min(17), Validators.max(17)]),
-      vehicleModel: new FormControl('', Validators.required),
-      Registration: new FormControl('', Validators.required),
-      warrantyPlan: new FormControl('', Validators.required)
-    })
   }
 
-  submitForm(){
-    this.isSubmitted = true;
-    if(!this.addVehicleForm.valid){
-      return false;
-    }
-    else{
-      const vehicle = {
-        VIN_Number: this.addVehicleForm.get('VINNum').value,
-        VehicleModel: this.addVehicleForm.get('vehicleModel').value,
-        Registration: this.addVehicleForm.get('Registration').value,
-        Warranty: this.addVehicleForm.get('warrantyPlan').value,
-        FleetID: ''
-      }
-
-      if(this.data != null || this.data != undefined){
-        vehicle.FleetID = this.data;
-      }
-      else{
-        vehicle.FleetID = '';
-      }
-
-      this.firestore.collection('Vehicle').add(vehicle).then(function(){
-        alert("New vehicle created successfully");
-      });
-    }
-     
-    this.router.navigate(['/tabs/view/fleet'], this.data);
-
-  }
 
   ngOnInit() {
-    this.addVehicleForm.setValue({VINNum: '', vehicleModel: '', Registration: '', warrantyPlan: ''});
-  }
-
-  get errorControl() {
-    return this.addVehicleForm.controls;
   }
 }
--- src/app/view-service-progress/view-service-progress.page.ts
+++ src/app/view-service-progress/view-service-progress.page.ts
@@ -13,45 +13,17 @@ import { Vehicle } from 'src/app/models/Vehicle';
 })
 export class ViewServiceProgressPage implements OnInit {
 
-  vehicles: Vehicle;
-  vehicle = {};
-  viewVehicleForm: FormGroup;
   isSubmitted = false;
   data: any;
-  maintenanceplanID: any;
 
   constructor(private route: ActivatedRoute, public fb: FormBuilder, public authService: AuthService, public firestore: AngularFirestore,
     public router: Router, public service: VehicleService) {
       this.route.params.subscribe(params => {
           this.data = params.id;
       });
-      this.viewVehicleForm = new FormGroup({
-        VINNum: new FormControl('', [Validators.required, Validators.min(17), Validators.max(17)]),
-        vehicleModel: new FormControl('', Validators.required),
-        Registration: new FormControl('', Validators.required),
-        warrantyPlan: new FormControl('', Validators.required)
-      })
      }
 
   ngOnInit() {
     
-  }
-
-  submitForm(){
-    this.service.getVehicle(this.data).valueChanges()
-    .subscribe(res =>{
-      this.maintenanceplanID = res['MaintenanceID'];
-    });
-    console.log(this.maintenanceplanID)
-    if(this.maintenanceplanID != null || this.maintenanceplanID != undefined){
-      this.router.navigate(['/tabs/view/maintenanceplan', '7jk7GWQB5eC6SdZuzU6P']);
-    }
-    else{
-      this.router.navigate(['/tabs/search/maintenanceplan']);
-    }
-  }
-
-  navToUpdate() {
-    this.router.navigate(['tabs/edit/vehicle', this.data]);
   }
 }
