8cd22dd8dd4a910812ff1198c56b607f2cf13de7
Albertus <u20487755@tuks.co.za>
2022-07-15 13:58:44 +0200

    create quote updated
    create quote ui

--- src/app/create-quote/create-quote.page.ts
+++ src/app/create-quote/create-quote.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder,FormGroup } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-quote',
   templateUrl: './create-quote.page.html',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateQuotePage implements OnInit {
 
-  constructor() { }
+QuoteForm: FormGroup;
+
+  constructor(public authService: AuthService,public router: Router) { }
 
   ngOnInit() {
   }
