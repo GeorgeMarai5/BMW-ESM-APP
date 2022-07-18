5d78b8d3963ea31ebf448ebae7d3cb98e3e284a7
Jordan <62015946+u19187654@users.noreply.github.com>
2022-07-18 17:43:37 +0200

    conclude service procedure implemneted

--- src/app/view-service/view-service.page.ts
+++ src/app/view-service/view-service.page.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { FormBuilder,FormGroup, FormControl, Validators, Form } from '@angular/forms';
-import{Service} from '../services/service.service';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  Form,
+} from '@angular/forms';
+import { Service } from '../services/service.service';
 import { Router, NavigationExtras } from '@angular/router';
 import { stringify } from 'querystring';
 import { CancelServicePage } from '../cancel-service/cancel-service.page';
-
+import { AlertController } from '@ionic/angular';
 // interface ServiceData {
 //   ServiceID: number;
 //   DealershipName: string;
 //   TeamName: string;
 //   ServiceType: string;
 //   ServiceStatus: string;
-  
+
 // }
 
-interface ServiceVehicles{
+interface ServiceVehicles {
   VehicleID: number;
   DealershipName: string;
   TeamName: string;
   ServiceTypeName: string;
   ServiceStatus: string;
-
 }
-
 
 @Component({
   selector: 'app-view-service',
@@ -31,20 +35,23 @@ interface ServiceVehicles{
   styleUrls: ['./view-service.page.scss'],
 })
 export class ViewServicePage implements OnInit {
+  serviceList = [];
 
-   serviceList = [];
-   
-   serviceForm: FormGroup;
-   services: Service;
-   searchTerm: string;
-   ServiceData: ServiceVehicles;
+  serviceForm: FormGroup;
+  services: Service;
+  searchTerm: string;
+  ServiceData: ServiceVehicles;
 
-  constructor(public authService: AuthService, private _service: Service, public router: Router, private fb: FormBuilder) { 
-
-   
+  constructor(
+    public authService: AuthService,
+    private _service: Service,
+    public router: Router,
+    private fb: FormBuilder,
+    private alertController: AlertController
+  ) {
     this.ServiceData = {} as ServiceVehicles;
-
   }
+
   // openDetailsWithState() {
   //   let navigationExtras: NavigationExtras = {
   //     state: {
@@ -61,7 +68,6 @@ export class ViewServicePage implements OnInit {
   //     ModelName: ['', [Validators.required]],
   //     Year: ['', [Validators.required]],
   // });
-
 
   // this._service.readService().subscribe(data => {
 
@@ -84,58 +90,60 @@ export class ViewServicePage implements OnInit {
       DealershipName: ['', [Validators.required]],
       TeamName: ['', [Validators.required]],
       ServiceType: ['', [Validators.required]],
-      ServiceStatus: ['', [Validators.required]]
-  });
+      ServiceStatus: ['', [Validators.required]],
+    });
 
-  this._service.getServices().subscribe(data => {
+    this._service.getServices().subscribe((data) => {
+      this.serviceList = data.map((e) => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          ServiceID: e.payload.doc.data()['ServiceID'],
+          DealershipName: e.payload.doc.data()['DealershipName'],
+          TeamName: e.payload.doc.data()['TeamName'],
+          ServiceType: e.payload.doc.data()['ServiceType'],
+          ServiceStatus: e.payload.doc.data()['ServiceStatus'],
+        };
+      });
+      console.log(this.serviceList);
+    });
 
-    this.serviceList = data.map(e => {
-      return {
-        id: e.payload.doc.id,
-        isEdit: false,
-        ServiceID: e.payload.doc.data()['ServiceID'],
-        DealershipName: e.payload.doc.data()['DealershipName'],
-        TeamName: e.payload.doc.data()['TeamName'],
-        ServiceType: e.payload.doc.data()['ServiceType'],
-        ServiceStatus: e.payload.doc.data()['ServiceStatus']
-      };
-    })
-    console.log(this.serviceList);
+    //     this._service.getService('LhV0lKeg0Cokxc0nrIDT').valueChanges()
+    //     .subscribe(res =>{
+    //     console.log(res)
+    //     this.serviceForm.setValue({
+    //       ServiceID: res['ServiceID'],
+    //       DealershipName: res['DealershipName'],
+    //       TeamName: res['TeamName'],
+    //       ServiceTypeName: res['ServiceTypeName'],
+    //       ServiceStatus: res['ServiceStatus']
+    //     })
+    //     });
 
-  });
+    //     this.serviceForm = this.fb.group({
+    //       ServiceID: ['', [Validators.required]],
+    //       DealershipName: ['', [Validators.required]],
+    //       TeamName: ['', [Validators.required]],
+    //       ServiceTypeName: ['', [Validators.required]],
+    //       ServiceStatus: ['', [Validators.required]]
+    //   });
+    // }
+    //   openDetailsWithState() {
+    //     let navigationExtras: NavigationExtras = {
+    //       state: {
+    //         id: 'LhV0lKeg0Cokxc0nrIDT'
+    //       }
+    //     };
+    //     this.router.navigate(['tabs/edit/vehicle'], navigationExtras);
+    //   }
+  }
+  async concludeServiceAlert() {
+    const alert = await this.alertController.create({
+      header: 'Conclude Service',
+      message: 'Are you sure you would like to end this service?',
+      buttons: ['Conclude', 'Back'],
+    });
 
-
-
-
-//     this._service.getService('LhV0lKeg0Cokxc0nrIDT').valueChanges()
-//     .subscribe(res =>{
-//     console.log(res)
-//     this.serviceForm.setValue({
-//       ServiceID: res['ServiceID'], 
-//       DealershipName: res['DealershipName'], 
-//       TeamName: res['TeamName'],
-//       ServiceTypeName: res['ServiceTypeName'],
-//       ServiceStatus: res['ServiceStatus']
-//     })
-//     });
-  
-//     this.serviceForm = this.fb.group({
-//       ServiceID: ['', [Validators.required]],
-//       DealershipName: ['', [Validators.required]],
-//       TeamName: ['', [Validators.required]],
-//       ServiceTypeName: ['', [Validators.required]],
-//       ServiceStatus: ['', [Validators.required]]
-//   });
-// }
-//   openDetailsWithState() {
-//     let navigationExtras: NavigationExtras = {
-//       state: {
-//         id: 'LhV0lKeg0Cokxc0nrIDT'
-//       }
-//     };
-//     this.router.navigate(['tabs/edit/vehicle'], navigationExtras);
-//   }
-
-
-
-  }}
+    await alert.present();
+  }
+}
