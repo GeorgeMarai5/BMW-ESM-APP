aada1dce8bc171c90f8fad06bfcf00ad6cd480d7
Jordan <62015946+u19187654@users.noreply.github.com>
2022-07-20 19:39:40 +0200

    capture feedback done

--- src/app/view-service-progress/view-service-progress.page.ts
+++ src/app/view-service-progress/view-service-progress.page.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
-import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { VehicleService } from 'src/app/services/vehicle.service';
 import { Vehicle } from 'src/app/models/Vehicle';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-view-service-progress',
@@ -12,18 +18,36 @@ import { Vehicle } from 'src/app/models/Vehicle';
   styleUrls: ['./view-service-progress.page.scss'],
 })
 export class ViewServiceProgressPage implements OnInit {
-
   isSubmitted = false;
   data: any;
 
-  constructor(private route: ActivatedRoute, public fb: FormBuilder, public authService: AuthService, public firestore: AngularFirestore,
-    public router: Router, public service: VehicleService) {
-      this.route.params.subscribe(params => {
-          this.data = params.id;
-      });
-     }
+  constructor(
+    private route: ActivatedRoute,
+    public fb: FormBuilder,
+    public authService: AuthService,
+    public firestore: AngularFirestore,
+    public router: Router,
+    public service: VehicleService,
+    private alertController: AlertController
+  ) {
+    this.route.params.subscribe((params) => {
+      this.data = params.id;
+    });
+  }
 
-  ngOnInit() {
-    
+  ngOnInit() {}
+
+  async provideFeedbackAlert() {
+    const alert = await this.alertController.create({
+      header: 'Feedback',
+      buttons: ['Submit', 'Cancel'],
+      inputs: [
+        {
+          placeholder: 'Please enter your feedback here...',
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
