939f4fbb53893f900b72811644601e1e3bd663b8
Mohitlala802 <u19207353@tuks.co.za>
2022-07-24 15:24:39 +0200

    Created Controller

---
+++ BMW-ESM-APP-API/BMW-ESM-APP-API/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMW_ESM_APP_API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LoginController : ControllerBase
+  {
+
+
+  }
+}
