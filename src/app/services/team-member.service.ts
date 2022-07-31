34fa49039c406c19b56c9683cf2882c5be1252e6
Mohitlala802 <u19207353@tuks.co.za>
2022-07-31 10:23:31 +0200

    Team/Team Member Services

--- src/app/services/team-member.service.ts
+++ src/app/services/team-member.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { employee } from '../models/Employee';
+import { Team } from '../models/Team';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamMemberService {
 
-  constructor() { }
+  apiUrl = 'https://localhost:44381/api/'
+
+  httpOptions ={
+    headers: new HttpHeaders({
+      ContentType: 'application/json'
+    })
+  }
+
+  constructor(private httpClient: HttpClient) {   
+  }
+
+  GetQuote(GetEmployee: employee){
+    return this.httpClient.post(`${this.apiUrl}EmployeeController/GetEmployee`,GetEmployee, this.httpOptions)
+  }
+
+  DeleteQuote(DeleteEmployee: employee){
+    return this.httpClient.delete<employee>(`${this.apiUrl}EmployeeController/DeleteEmployee`)
+  }
+
+  CreateQuote(CreateEmployee: employee){
+
+    return this.httpClient.post<employee>(`${this.apiUrl}EmployeeController/CreateEmployee`,CreateEmployee, this.httpOptions)
+  }
+
+  
+  UpdateQuote(UpdateEmployee: employee){
+
+    return this.httpClient.post<employee>(`${this.apiUrl}EmployeeController/CreateEmployee`,UpdateEmployee, this.httpOptions)
+  }
+
 }
--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Team } from '../models/Team';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TeamService {
+export class TeamMemberService {
 
-  constructor() { }
+  apiUrl = 'https://localhost:44381/api/'
+
+  httpOptions ={
+    headers: new HttpHeaders({
+      ContentType: 'application/json'
+    })
+  }
+
+  constructor(private httpClient: HttpClient) {   
+  }
+
+  GetQuote(GetTeam: Team){
+    return this.httpClient.post(`${this.apiUrl}TeamController/GetTeam`,GetTeam, this.httpOptions)
+  }
+
+  DeleteQuote(DeleteTeam: Team){
+    return this.httpClient.delete<Team>(`${this.apiUrl}TeamController/DeleteTeam`)
+  }
+
+  CreateQuote(CreateTeam: Team){
+
+    return this.httpClient.post<Team>(`${this.apiUrl}TeamController/CreateTeam`,CreateTeam, this.httpOptions)
+  }
+
+  
+  UpdateQuote(UpdateTeam: Team){
+
+    return this.httpClient.post<Team>(`${this.apiUrl}TeamController/CreateTeam`,UpdateTeam, this.httpOptions)
+  }
+
 }
