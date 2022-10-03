6c63096c0bb30f2f6c2f7bb7e2bade5f028165b9
GeorgeMarai5 <u20774312@tuks.co.za>
2022-10-03 16:16:21 +0200

    Fixing Jeff

--- src/app/create-service/create-service.page.ts
+++ src/app/create-service/create-service.page.ts
@@ -94,7 +94,26 @@ this.getTeam();
 
 this.getType();
     
+var coll = document.getElementsByClassName("collapsible");
+    var i;
+    let up = document.getElementById('up');
+    let down = document.getElementById('down');
 
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+          down.style.display = "none";
+          up.style.display = "block";
+        } else {
+          content.style.display = "block";
+          up.style.display = "none";
+          down.style.display = "block";
+        }
+      });
+    }
    
   }
 
--- src/app/edit-assigned-part/edit-vehicle-part.page.ts
+++ src/app/edit-assigned-part/edit-vehicle-part.page.ts
@@ -72,6 +72,27 @@ export class EditVehiclePartPage implements OnInit {
       partStock: res['PartInStock']
     })
     });
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    let up = document.getElementById('up');
+    let down = document.getElementById('down');
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+          down.style.display = "none";
+          up.style.display = "block";
+        } else {
+          content.style.display = "block";
+          up.style.display = "none";
+          down.style.display = "block";
+        }
+      });
+    }
   }
 
   async showHelp(){
